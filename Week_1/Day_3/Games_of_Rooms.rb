b79863f3e0f5b81be50b66385d0ea4fe9ef1b504
MFigand <m.figand@gmail.com>
2016-02-05 09:44:50 +0100

    Exercises of Day 4

--- Week_1/Day_3/Games_of_Rooms.rb
+++ Week_1/Day_3/Games_of_Rooms.rb
@@ -14,6 +14,7 @@ class Game
     elsif position == "0" && choose.upcase == "EXIT"
     else
       puts "Aun es pronto, entra y disfruta de la fiesta"
+      puts @roomArray[0].description
       place1(gets.chomp,"0")
     end
   end
@@ -30,8 +31,9 @@ class Game
       place3(newChoose,place)
     elsif position == "1" && choose.upcase == "EXIT"
       else
-      puts "Se que es una dificl desicion pero debes elegir, yo recomiendo la birra"
-      place1(gets.chomp,"1")
+      puts "Se que es una dificl decision pero debes elegir, yo recomiendo la birra"
+      puts @roomArray[1].description
+      place2(gets.chomp,"1")
     end
   end
   def place3(choose, position)
@@ -53,7 +55,8 @@ class Game
     elsif position == "2" && choose.upcase == "EXIT"
       else
       puts "Esa no es una opcion, vete por una birra mientras lo piensas"
-      place1(gets.chomp,"2")
+      puts @roomArray[2].description
+      place3(gets.chomp,"2")
     end
   end
   def place4(choose, position)
@@ -70,7 +73,8 @@ class Game
     elsif position == "3" && choose.upcase == "EXIT"
       else
       puts "Ese sitio no es una opción, te recomiendo ir al aseo y puedas beber más birras"
-      place1(gets.chomp,"3")
+      puts @roomArray[3].description
+      place4(gets.chomp,"3")
     end
   end
   def place5(choose, position)
@@ -81,8 +85,9 @@ class Game
       place2(newChoose,place)
     elsif position == "4" && choose.upcase == "EXIT"
       else
-      puts "No, inoves. Lo mejor es ir a la barra por mas birra"
-      place1(gets.chomp,"4")
+      puts "No, innoves. Lo mejor es ir a la barra por mas birra"
+      puts @roomArray[4].description
+      place5(gets.chomp,"4")
     end
   end
   def place6(choose, position)
@@ -94,7 +99,8 @@ class Game
     elsif position == "5" && choose.upcase == "EXIT"
       else
       puts "Ya has vaciado el tanque, cruza por la sala de DJ y ve por mas birras"
-      place1(gets.chomp,"5")
+      puts @roomArray[5].description
+      place6(gets.chomp,"5")
     end
   end
 
@@ -114,7 +120,7 @@ roomArray = [
   room3 = Room.new(["D","B","S"], "Estas junto al escenario, escribe D para ir a la sala de DJ, B para ir a la Barra o S para salir del Bar"),
   room4 = Room.new(["C","A"], "Estas en la sala del Dj, escrib C para ir al concierto o A para ir al aseo"),
   room5 = Room.new(["V"], "Te has terminado tu birra, escribe V vuelve a la barra y pide otra"),
-  room6 = Room.new(["D"], "Estas en el aseo, escribe D para volver a la sale del DJ")
+  room6 = Room.new(["D"], "Estas en el aseo, escribe D para volver a la sala del DJ")
 ]
 game = Game.new(roomArray)
 
