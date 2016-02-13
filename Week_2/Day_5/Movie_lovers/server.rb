47a34166f9e16936e717ca6ed7cc068b4d24f73b
MFigand <m.figand@gmail.com>
2016-02-13 17:46:05 +0100

    Quiz complete, need style format

--- Week_2/Day_5/Movie_lovers/server.rb
+++ Week_2/Day_5/Movie_lovers/server.rb
@@ -2,7 +2,8 @@ require "sinatra"
 require "sinatra/reloader" 
 require "Imdb"
 require_relative "./lib/movie_lovers.rb"
-# enable(:sessions)
+# require_relative "./lib/Fakemovie_lovers.rb"
+
 
 get "/" do 
   erb(:home)
@@ -12,5 +13,16 @@ post "/search" do
   s = Search.new(params[:searchWord])
   @nineMovieArray = s.selector
   @year = (@nineMovieArray.sample).year
+  # binding.pry
   erb(:posters)
+end
+
+post "/answer" do
+  if params[:choosenYear] == params[:correctYear] 
+   "correct"
+    # erb(:correct)
+  else
+    "incorrect"
+    # erb(:incorrect)
+  end
 end
