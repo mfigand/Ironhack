1536a3f898981134644f586e9e250512339e8326
MFigand <m.figand@gmail.com>
2016-02-19 17:42:53 +0100

    Half clone Twitter done

--- Week_3/Day_5/Quiz_Game/quiz_game.js
+++ Week_3/Day_5/Quiz_Game/quiz_game.js
@@ -5,31 +5,64 @@ var Question = function(text, answer, id){
   this.answer = answer.toLowerCase();
   this.id = id;
   this.analize = function(userAnswer){
-    if (this.answer === userAnswer){console.log("Correct!!");
-    } else {console.log("Incorrect") ;}
+    if (this.answer === userAnswer){
+      console.log("Correct!!");
+      quiz.nextQuestion(this.id)
+    } else {
+      console.log("Incorrect");
+      quiz.repetQuestion(this.id)
+    }
   };
 
 }
 
-// var Quiz = function(){
-//   userAnswer,question
-//
-// }
+var Quiz = function(questArray){
+  this.nextQuestion = function(id){
+    read(options2, startGame)
+  }
+  this.repetQuestion = function(id){
+    read(options1, startGame)
+  }
+
+}
 
 var quest1 = new Question("Worldcup champion 1930?\n>", "uruguay", 1)
 var quest2 = new Question("Worldcup country 1986?\n>", "mexico", 2)
 var quest3 = new Question("Worldcup champion 1954?\n>", "alemania federal", 3)
-// var quiz = new Quiz()
+var questArray = [quest1,quest2,quest3];
+var correctAnswerArray = [];
+var quiz = new Quiz(questArray)
+var actualQuestion = questArray[Math.floor(Math.random()*questArray.length)]
+var actualId = actualQuestion.id;
 
-options = {
-    prompt: "You have 10 seconds to answer\n>"+quest1.text,
+function checkQuest(id) {
+  for (i=0; i<correctAnswerArray.length; i++){
+      if (correctAnswerArray[i] != id){
+      correctAnswerArray.push(id);
+      questArray.splice(i,1)
+      }
+  }
+ if (questArray.length === 0){
+   return console.log("You Win");
+ }
+ else {
+   return actualQuestion = questArray[Math.floor(Math.random()*questArray.length)]
+ }
+}
+
+options1 = {
+    prompt: "You have 10 seconds to answer\n>"+checkQuest(1).text,
     timeout: 10000
 }
 
-read(options, startGame)
+options2 = {
+    prompt: "Again you have 10 seconds to answer\n>"+checkQuest(actualId).text,
+    timeout: 10000
+}
+
+read(options1, startGame)
 
 function startGame (err, userAnswer){
     // console.log("Your name is: " + name)
-    quest1.analize(userAnswer)
-
+    actualQuestion.analize(userAnswer)
 }
