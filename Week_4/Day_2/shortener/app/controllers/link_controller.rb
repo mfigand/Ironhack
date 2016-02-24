a6de3375afbbf889ff2b177d3b9ba44f388f276d
MFigand <m.figand@gmail.com>
2016-02-24 08:42:04 +0100

    Url Shortener

--- Week_4/Day_2/shortener/app/controllers/link_controller.rb
+++ Week_4/Day_2/shortener/app/controllers/link_controller.rb
@@ -2,7 +2,9 @@ class LinkController < ApplicationController
 
   def shortlink
     @originallink = params[:link][:originallink]
-    link = Link.create(originallink: @originallink, shortlink: "345")
+    url_user = check_http(@originallink)
+    randomKey = [*('A'..'Z'),*('a'..'z')].shuffle[0,3].join
+    link = Link.create(originallink: url_user, shortlink: randomKey)
     link.save
     redirect_to "/#{link.shortlink}"
   end
