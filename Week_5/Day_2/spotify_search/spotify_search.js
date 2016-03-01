ee3fb8dcd8627ea6e097f78f2bde86e600cbce02
MFigand <m.figand@gmail.com>
2016-03-01 17:37:36 +0100

    Show the albums of the artists

--- Week_5/Day_2/spotify_search/spotify_search.js
+++ Week_5/Day_2/spotify_search/spotify_search.js
@@ -1,5 +1,6 @@
 $(document).on('ready', function(){
   $('.js-search').on('click',function () {
+    $('.list_result').empty();
     var artist = $(".input").val();
     var request = $.get('https://api.spotify.com/v1/search?type=artist&query='+artist);
     request.done(handleResponse);
@@ -7,19 +8,46 @@ $(document).on('ready', function(){
   });
 });
 
-
-
-
 function handleResponse (response){
   console.log(response);
 
     $.each(response.artists.items,function(index,val) {
-     $('.list_result').append("<li>"+"artist:"+val.name+"</li>");
+      if (val.images.length === 0){
+        $('.list_artist').append("<li class='li_artist' id="+val.id+">Artist: "+val.name+"  NO IMAGE ALBUM</li>");
+      }
+      else {
+        $('.list_artist').append(
+          "<li class='li_artist' id="+val.id+" >"+"artist: "+val.name+"</li>"
+          +"<img src='"+val.images[0].url+"'/>"
+
+        );
+
+
+      }
     });
-    // $(".list_result").append("<li>"+"artist:"+response.artists.items[0].name+"</li>");
+    $('.li_artist').on('click', handleClick );
+
 };
 
 function handleError (error){
   console.log(error);
-  $(".error").text("Sorry an error occurred");
+  $(".error").text("Sorry an error occurred!!");
+};
+
+var handleClick = function(event){
+  console.log('llego',$(this).prop('id'));
+  window.P = $(event.currentTarget)
+  var request = $.get('https://api.spotify.com/v1/artists/'+$(this).prop('id')+'/albums');
+  request.done(show_albums);
+  request.fail(handleError);
+}
+
+function show_albums (albums){
+  console.log(albums)
+  $.each(albums.items,function(index,val) {
+    $(".list_album").append(
+      "<li class='li_album' id="+val.id+" >"
+      +"Album: "+val.name
+      +"</li>");
+    })
 }
